2e2fa5f994aaac849654bee12c928d73a018ef8c
Semarc <sebastian.krusche@gmx.de>
2020-11-20 19:34:46 +0100

    Slight Fix

--- C#/Sourcen/DatabaseProject/Program.cs
+++ C#/Sourcen/DatabaseProject/Program.cs
@@ -11,7 +11,6 @@ namespace DatabaseProject
 		[STAThread]
 		static void Main()
 		{
-			Application.SetHighDpiMode(HighDpiMode.SystemAware);
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 			Application.Run(new MainForm());
